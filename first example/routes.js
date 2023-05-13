f667d43b04b36f7ab29cda1c246a7f3ba1f51998
jake dapper <jakedapper@gmail.com>
2023-05-12 21:03:00 -0500

    modular organization

--- first example/routes.js
+++ first example/routes.js
@@ -1,10 +1,10 @@
-const http = require('http');
-const fs = require('fs');
+const fs = require('fs')
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === '/') {
+const requestHandler = (req, res) => {
+    const url = req.url
+    const method = req.method
+
+if (url === '/') {
     res.write('<html>');
     res.write('<head><title>Enter Message</title><head>');
     res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
@@ -20,11 +20,12 @@ const server = http.createServer((req, res) => {
     return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
+      fs.writeFile('message.txt', message, (err) => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
     });
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
   }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
@@ -32,6 +33,6 @@ const server = http.createServer((req, res) => {
   res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
   res.write('</html>');
   res.end();
-});
+}
 
-server.listen(3000);
+module.exports = requestHandler

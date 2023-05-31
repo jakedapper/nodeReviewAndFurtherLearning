3efa434645bc0d24052b20681d227923c3dad9c8
jake dapper <jakedapper@gmail.com>
2023-05-31 13:43:07 -0500

    update

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const express = require('express');
 const path = require('path');
 
 const express = require('express');
@@ -6,13 +5,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {

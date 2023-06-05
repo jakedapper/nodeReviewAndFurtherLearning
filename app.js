d46a9ec4cc5e60908d766f986464251a173a023c
jake dapper <jakedapper@gmail.com>
2023-06-05 09:54:15 -0500

    more pug more features

--- app.js
+++ app.js
@@ -5,22 +5,20 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set('view engine', 'pug')
-app.set('views', 'views')
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-//body parser had to be installed
 app.use(bodyParser.urlencoded({extended: false}));
-//statically serving local files
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);

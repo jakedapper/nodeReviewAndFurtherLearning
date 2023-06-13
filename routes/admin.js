327f123c8eed8f7fba2b0d4eae30c896497e4bc9
jake dapper <jakedapper@gmail.com>
2023-06-12 20:03:54 -0500

    more MVC cnfig

--- routes/admin.js
+++ routes/admin.js
@@ -6,10 +6,8 @@ const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-// /admin/add-product => GET
 router.get('/add-product', productsController.getAddProduct);
 
-// /admin/add-product => POST
 router.post('/add-product', productsController.postAddProduct);
 
 module.exports = router;
